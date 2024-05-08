ff8bb753c1344a6ef9675526ffb5a888fb2b6589
rafael katri <rkatri20033@gmail.com>
2024-05-08 10:53:08 -0300

    update cookies

--- popup.js
+++ popup.js
@@ -1,10 +1,21 @@
-// popup.js
 document.addEventListener('DOMContentLoaded', () => {
     const infoElement = document.getElementById('info');
-    browser.cookies.getAll({}).then(cookies => {
-        cookies.forEach(cookie => {
-          infoElement.textContent += `Domain: ${cookie.domain}, Name: ${cookie.name}, Value: ${cookie.value}\n`;
+
+    // Function to update the cookies displayed in the popup
+    function updateCookiesDisplay() {
+        browser.cookies.getAll({}).then(cookies => {
+            infoElement.textContent = `${cookies.length}\n`; // Clear the existing content
+            cookies.forEach(cookie => {
+                infoElement.textContent += `Domain: ${cookie.domain}, Name: ${cookie.name}, Value: ${cookie.value}\n`;
+            });
         });
-      });
-  });
-  
+    }
+
+    // Update the cookies display initially
+    updateCookiesDisplay();
+
+    // Listen for changes to cookies and update the display accordingly
+    browser.cookies.onChanged.addListener((changeInfo) => {
+        updateCookiesDisplay();
+    });
+});
